54c9dcc63e38338f75eed17686bc8e53604f21e2
Avanish-Shukla <savanish610@gmail.com>
2024-06-25 17:22:00 +0530

    Added few more topics

---
+++ node-modules/package/index.js
@@ -0,0 +1,11 @@
+
+function Hello(){
+    console.log("Hello");
+}
+
+Hello()
+//starting point of our package
+
+// use npm init or npm init --yes to create a package.json file with manual or default template
+
+// to install a old or new version just type npm install packagename@4.13.12 so it will install the "packagename" package with version 4.13.12 and in dependency it will be overwritten
